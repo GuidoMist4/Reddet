0a5b6a5ed870ee730aee0d5e7a74dd5af1fb3d1b
claudio ciaravolo <claudiociaravolo00@gmail.com>
2022-11-25 10:48:12 +0100

    aggiunto repository di post, subReddet e utente

---
+++ src/main/java/net/bcsoft/com/Reddet/repository/SubRedditRepo.java
@@ -0,0 +1,7 @@
+package net.bcsoft.com.Reddet.repository;
+
+import net.bcsoft.com.Reddet.model.SubReddet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubRedditRepo extends JpaRepository<SubReddet, Long> {
+}
