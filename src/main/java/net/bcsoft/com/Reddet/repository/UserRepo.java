f5dc7358b502ed22045abfa18c1000c3ed919d03
Cap0 <angelo_capodanno@yahoo.it>
2022-11-28 11:12:27 +0100

    Ilario vattimm a Claudio <3

--- src/main/java/net/bcsoft/com/Reddet/repository/UserRepo.java
+++ src/main/java/net/bcsoft/com/Reddet/repository/UserRepo.java
@@ -3,5 +3,5 @@ package net.bcsoft.com.Reddet.repository;
 import net.bcsoft.com.Reddet.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UtenteRepo extends JpaRepository<User,Long> {
+public interface UserRepo extends JpaRepository<User,Long> {
 }
