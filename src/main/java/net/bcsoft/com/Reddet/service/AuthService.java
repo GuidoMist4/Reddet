287ba933be2966adeb2fa332bca6eb975bfcf417
Cap0 <angelo_capodanno@yahoo.it>
2022-11-28 12:45:09 +0100

    Claudio CULO <3

--- src/main/java/net/bcsoft/com/Reddet/service/AuthService.java
+++ src/main/java/net/bcsoft/com/Reddet/service/AuthService.java
@@ -2,11 +2,14 @@ package net.bcsoft.com.Reddet.service;
 
 import net.bcsoft.com.Reddet.DTO.RegisterRequest;
 import net.bcsoft.com.Reddet.model.User;
+import net.bcsoft.com.Reddet.model.VerificationToken;
 import net.bcsoft.com.Reddet.repository.UserRepo;
+import net.bcsoft.com.Reddet.repository.VerificationTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import javax.transaction.Transactional;
 import java.time.Instant;
+import java.util.UUID;
 
 public class AuthService {
 
@@ -27,6 +30,16 @@ public class AuthService {
         user.setEnabled(false);
         userRepo.save(user);
 
+    }
+
+    @Autowired
+    VerificationTokenRepository verificationTokenRepository;
+    private String GenerateVerificationToken(User user){
+        String token= UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setUser(user);
+        verificationToken.setToken(token);
+        return token;
     }
 
 
