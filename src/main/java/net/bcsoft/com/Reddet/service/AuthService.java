10627772ee1e3ad6ea650c5fb748f9af28825dff
claudio ciaravolo <claudiociaravolo00@gmail.com>
2022-11-28 11:24:32 +0100

    aggiunto repository di post, subReddet e utente

--- src/main/java/net/bcsoft/com/Reddet/service/AuthService.java
+++ src/main/java/net/bcsoft/com/Reddet/service/AuthService.java
@@ -2,17 +2,24 @@ package net.bcsoft.com.Reddet.service;
 
 import net.bcsoft.com.Reddet.DTO.RegisterRequest;
 import net.bcsoft.com.Reddet.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.transaction.Transactional;
 import java.time.Instant;
 
 public class AuthService {
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Transactional
     public void SignUp(RegisterRequest registerRequest){
         User user = new User();
 
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
 
         user.setCreated(Instant.now());
 
