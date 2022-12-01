4c7ff605590fc34cf277df07330304605779b218
claudio ciaravolo <claudiociaravolo00@gmail.com>
2022-12-01 12:41:15 +0100

    aggiunta controller signup

--- src/main/java/net/bcsoft/com/Reddet/ReddetApplication.java
+++ src/main/java/net/bcsoft/com/Reddet/ReddetApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
 @EnableAsync
+@SpringBootApplication
 public class ReddetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReddetApplication.class, args);
 	}
+
 }
---
+++ src/main/java/net/bcsoft/com/Reddet/security/JwtProvider.java
@@ -0,0 +1,42 @@
+package net.bcsoft.com.Reddet.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+@AllArgsConstructor
+public class JwtProvider {
+
+    private JwtEncoder jwtEncoder;
+
+    @Value("${jwt.expiration.time}")
+    private Long jwtExpirationInMillis;
+
+    public String generateToken(Authentication authentication){
+       User user=(User) authentication.getPrincipal();
+       return generateTokenWithUsername(user.getUsername());
+    }
+
+    public String generateTokenWithUsername(String username){
+        JwtClaimsSet jwtClaimsSet= JwtClaimsSet.builder()
+                .issuer("Self.")
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plusMillis(jwtExpirationInMillis))
+                .subject(username)
+                .claim("scope","ROLE_USER")
+                .build();
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
+    }
+
+    public Long getJwtExpirationInMillis(){
+        return jwtExpirationInMillis;
+    }
+}
