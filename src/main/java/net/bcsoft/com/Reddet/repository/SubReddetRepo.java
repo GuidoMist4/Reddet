1656026cedd4c339da5eb0dfda5ef6459bc696a0
claudio ciaravolo <claudiociaravolo00@gmail.com>
2022-12-05 11:38:11 +0100

    aggiunta metodo per ritorno del subreddet tramite passaggio indice

--- src/main/java/net/bcsoft/com/Reddet/repository/SubReddetRepo.java
+++ src/main/java/net/bcsoft/com/Reddet/repository/SubReddetRepo.java
@@ -1,9 +1,13 @@
 package net.bcsoft.com.Reddet.repository;
 
+import net.bcsoft.com.Reddet.DTO.SubReddetDTO;
 import net.bcsoft.com.Reddet.model.SubReddet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SubReddetRepo extends JpaRepository<SubReddet,Long> {
+
 }
