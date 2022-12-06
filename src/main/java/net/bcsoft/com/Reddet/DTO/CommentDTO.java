17edcd57ef721c0625d85fcc1f436d4c2f311da8
Cap0 <angelo_capodanno@yahoo.it>
2022-12-06 10:28:12 +0100

    Vincenza > Umanità <3

--- src/main/java/net/bcsoft/com/Reddet/DTO/CommentDTO.java
+++ src/main/java/net/bcsoft/com/Reddet/DTO/CommentDTO.java
@@ -1,4 +1,20 @@
 package net.bcsoft.com.Reddet.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentDTO {
+
+    private long id;
+    private long postId;
+    private Instant creationDate;
+    private String text;
+    private String username;
+
 }
