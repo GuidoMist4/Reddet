1976579a1dcebec61f3940737ae5551787cac13d
claudio ciaravolo <claudiociaravolo00@gmail.com>
2022-12-14 10:11:30 +0100

    claudio

--- reddetFrontEnd/src/app/app-routing.module.ts
+++ reddetFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< Updated upstream
 
 const routes: Routes = [];
+=======
+import { SignupComponent } from './auth/signup/signup.component';
+
+const routes: Routes = [{path:'sign-up',component:SignupComponent}];
+>>>>>>> Stashed changes
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ reddetFrontEnd/src/app/auth/signup/signup-request.payload.ts
@@ -0,0 +1,1 @@
+export interface SignupRequestPayload{username:string;password:string;email:string}
